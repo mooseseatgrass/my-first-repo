701200237ca4d52f448fd1c5969a284b92d34910
mooseseatgrass <skylover29@gmail.com>
2020-01-29 13:49:26 -0500

    finish hw

---
+++ Js Demo/firstjava.js
@@ -0,0 +1,19 @@
+// java homework 1
+
+var skate = "awesome";
+var wheels = 8;
+
+var x = (skate * wheels);
+
+console.log(x);
+
+
+console.log(skate * wheels);
+
+2 + true;
+
+console.log(2 + true);
+
+
+
+
