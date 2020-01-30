a6c65426e99e0d3f4202c85ed1387beeef8309b5
mooseseatgrass <skylover29@gmail.com>
2020-01-30 14:29:03 -0500

    did conditional statement hw

--- Js Demo/firstjava.js
+++ Js Demo/firstjava.js
@@ -10,9 +10,99 @@ console.log(x);
 
 console.log(skate * wheels);
 
-2 + true;
-
 console.log(2 + true);
+
+/* 
+infinint loop, can also happen when diving by zero. if inside is false it won't run
+while(true){
+    console.log("hello");
+
+}
+
+var count = 15;
+var doCount = 15;
+
+function print(){
+    while(count > 10){
+        console.log("test";)
+        counr --;
+    }
+}do{
+    console.log("hi");
+        doCount--;
+}
+while(doCount > 20);
+count = 200;
+}
+
+*/
+
+// Conditional statements HW
+var count= 2;
+
+count = count + 5;
+console.log(count);
+
+count = count * 2;
+console.log(count);
+
+count = count % 3;
+// remainder of 2 possilbly
+console.log(count);
+//remainder of 0
+count = count % 2;
+console.log(count)
+
+if(count % 2 == 0){
+    console.log("True");
+
+} else{
+    console.log("False");
+}
+
+if (count > 0 && count < 0){
+    console.log("biggerThanZero");
+}
+
+if (count == 10 || count >= 5){
+    console.log("equals10");
+}
+/*
+if(1 === 1){
+    console.log(gravy);
+}
+if(count > 0){
+    console.log(biscuits);
+}
+if (count < 9){
+    console.log(fork);
+
+}
+if(1 === 1 && count > 0 && count < 9){
+    console.log(completed);
+}
+*/
+
+// The conditional statements put together save space but there are not as many path choices for what happens
+
+// Loop exercise HW
+var long = 3
+
+function print(){
+    while(long < 50);
+    long++;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
