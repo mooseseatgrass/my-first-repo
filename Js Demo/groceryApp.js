b71befc28bcaeee3d02656eb5741dac5e3a18480
mooseseatgrass <skylover29@gmail.com>
2020-02-03 16:40:15 -0500

    iticial commit

---
+++ Js Demo/groceryApp.js
@@ -0,0 +1,27 @@
+var groceryInv = [
+    [10, "Bananas"], 
+    [4, "Onions"], 
+    [8, "Tomatoes"], 
+    [1, "Sugar"], 
+    [29, "Apples"], 
+    [9, "Oatmeal"], 
+    [7, "Salt"], 
+    [83, "Pepper"], 
+    [37, "Hot sauce"], 
+    [3, "Litter"]
+];
+
+// var inNum = 
+
+function addInv(){
+    let input = document.getElementById("userInput").value;
+    fruits.splice(inNum, count, i++);
+}
+
+function subInv(){
+
+}
+
+function outInv(){
+
+}
