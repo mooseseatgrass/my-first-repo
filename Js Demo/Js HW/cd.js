a1b133cd986a6a4d24fa1d3d4fb52402c865efa2
mooseseatgrass <skylover29@gmail.com>
2020-02-21 10:35:30 -0500

    fixed clock

--- Js Demo/Js HW/cd.js
+++ Js Demo/Js HW/cd.js
@@ -1,28 +1,44 @@
-
-function countDown(){
-    var today = new Date();
-    var eventDate = new Date("October 31, 2020 00:00:00");
-
-    var currentTime = today.getTime();
-    var eventTime = eventDate.getTime();
-
-    var remTime = eventTime - currentTime;
-
-    var sec = Math.floor(remTime/1000);
-    var min = Math.floor(sec/60);
-    var hour = Math.floor(min/60);
-    var days = Math.floor(hour/24);
-
-    hour = hour % 24;
-    min%=60;
-    sec%=60;
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hour").innerHTML = hour;
-    document.getElementById("min").innerHTML = min;
-    document.getElementById("sec").innerHTML = sec;
-
-    setTimeout(countDown,1000);
+function updateTimer(deadline){
+    var time = deadline - new Date();
+    return {
+        // maybe could add weeks and months with 1000*60*60*24*7 days in week, 52 weeks in year, 12 months in year, use %?
+        'days': Math.floor( time/(1000*60*60*24) ),
+        'hours': Math.floor( (time/(1000*60*60)) %24 ),
+        'minutes': Math.floor((time/100/60)%60),
+        'seconds': Math.floor((time/1000)%60),
+        'total': time
+    };
 }
 
-countDown();
+function startTimer(id, deadline){
+    var timerInterval = setInterval(function(){
+    var input = document.getElementById(id);
+    var timer = updateTimer(deadline);
+
+    // clock.innerHTML = '<span>' + timer.days + '</span>'
+    //                 + '<span>' + timer.hours + '</span>'
+    //                 + '<span>' + timer.minutes + '</span>'
+    //                 + '<span>' + timer.seconds + '</span>'
+    if(input.id == "days")
+    input.innerHTML = timer.days
+
+    if(input.id == "hours")
+    input.innerHTML = timer.hours
+
+    if(input.id == "minutes")
+    input.innerHTML = timer.minutes
+
+    if(input.id == "seconds")
+    input.innerHTML = timer.seconds
+
+    }, 1000);
+}
+
+window.onload = function(){
+    var deadline = new Date("October 31, 2020 00:00:00");
+    startTimer("days", deadline);
+    startTimer("hours", deadline);
+    startTimer("minutes", deadline);
+    startTimer("seconds", deadline);
+};
+
